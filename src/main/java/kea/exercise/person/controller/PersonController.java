90f94db9c710e63dd9a49473b69173ef674fe71d
gustavwiese <gusse12345@gmail.com>
2024-02-12 11:04:24 +0100

    commit 4

--- src/main/java/kea/exercise/person/controller/PersonController.java
+++ src/main/java/kea/exercise/person/controller/PersonController.java
@@ -3,9 +3,12 @@ package kea.exercise.person.controller;
 import kea.exercise.person.model.Person;
 import kea.exercise.person.repository.PersonRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class PersonController {
@@ -19,7 +22,14 @@ public class PersonController {
     @GetMapping("/persons")
     public List<Person>getAllPersons(){
 
-        List<Person> persons = personRepository.findAll();
-        return persons;
+        return personRepository.findAll();
+    }
+
+    @GetMapping("/persons/{id}")
+    public Person getPerson(@PathVariable int id){
+
+        Optional<Person> person = personRepository.findById(2);
+
+        return person.orElse(null);
     }
 }
--- src/main/java/kea/exercise/person/repository/PersonRepository.java
+++ src/main/java/kea/exercise/person/repository/PersonRepository.java
@@ -3,8 +3,8 @@ package kea.exercise.person.repository;
 import kea.exercise.person.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PersonRepository extends JpaRepository<Person, Integer> {
-
-
 
 }
