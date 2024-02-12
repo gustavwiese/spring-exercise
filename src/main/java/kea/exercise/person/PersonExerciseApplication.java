d181b55e505d1913b80f1ccbc99e63c6f0e123d5
gustavwiese <gusse12345@gmail.com>
2024-02-12 09:58:38 +0100

    first commit

---
+++ src/main/java/kea/exercise/person/PersonExerciseApplication.java
@@ -0,0 +1,13 @@
+package kea.exercise.person;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonExerciseApplication.class, args);
+	}
+
+}
